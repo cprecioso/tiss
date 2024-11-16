adf3a02d5817d36ad97257406b0e872e37e234de
Carlos Precioso <github@precioso.design>
2024-11-16 10:58:11 +0100

    Setup build

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { versions } from "node:process";
+import semver from "semver";
+import { defineConfig } from "tsup";
+
+const NODE_MAJOR = semver.parse(versions.node);
+
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  outDir: "./dist",
+  clean: true,
+
+  platform: "node",
+  target: `node${NODE_MAJOR}`,
+  format: ["esm"],
+  dts: true,
+});
