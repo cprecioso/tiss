b160cec36456740677328bab8ec853ecda1eed7a
Carlos Precioso <github@precioso.design>
2024-11-17 12:09:04 +0100

    Expose better schemas

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,7 @@
-import { ConfigInput } from "./schemas/config";
+import type { ConfigInput as Config } from "./schemas/config";
+import type { HandlerInput as Handler } from "./schemas/handler";
 
-export const defineConfig = (config: ConfigInput) => config;
+export const defineConfig = (config: Config): Config => config;
+export const defineHandler = (handler: Handler): Handler => handler;
 
-export type { ConfigInput as Config } from "./schemas/config";
+export type { Config, Handler };
